32537b01ee474a0c223385de6293a3ee3fb728b1
Tabbe Maas <tabbemaas@gmail.com>
2022-11-28 13:16:44 +0100

    exercise 2

---
+++ src/ArrayBuilder.java
@@ -0,0 +1,15 @@
+public class ArrayBuilder {
+    private int[] numbers;
+
+    public int[] getNumbers() {
+        return numbers;
+    }
+
+    public void setNumbers(int[] numbers) {
+        this.numbers = numbers;
+    }
+
+    public ArrayBuilder(int[] numbers) {
+        this.numbers = numbers;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        ArrayBuilder array = new ArrayBuilder(new int[] {1,2,3,4,5});
+        for (int number : array.getNumbers()) {
+            System.out.println(number);
+        }
+        for (int i = 0; i < array.getNumbers().length; i++) {
+            System.out.println(array.getNumbers()[i]);
+        }
+    }
+}
